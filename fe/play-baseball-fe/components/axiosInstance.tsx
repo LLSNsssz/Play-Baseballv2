ab8102cf0cc67a5258d0105c3b63a4698655638e
LLSNsssz <seungjang93@gmail.com>
2024-10-01 21:34:35 +0900

    Refactor: 토큰 추출시 공백 제거

--- fe/play-baseball-fe/components/axiosInstance.tsx
+++ fe/play-baseball-fe/components/axiosInstance.tsx
@@ -30,14 +30,17 @@ axiosInstance.interceptors.response.use(
     (response: AxiosResponse) => {
         const newAccessToken = response.headers['authorization'];
         if (newAccessToken) {
-            localStorage.setItem('Authorization', newAccessToken);
-            axiosInstance.defaults.headers.common['Authorization'] = newAccessToken;
+            const tokenWithoutBearer = newAccessToken.replace('Bearer ', '');
+            localStorage.setItem('Authorization', tokenWithoutBearer);
+            axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${tokenWithoutBearer}`;
         }
         return response;
     },
     (error: AxiosError) => {
-        // 여기서는 401 에러 처리를 제거합니다.
-        // 서버가 자동으로 새 액세스 토큰을 제공하므로 클라이언트에서 추가 처리가 필요 없습니다.
+        if (error.response?.status === 401) {
+            localStorage.removeItem('Authorization');
+            window.location.href = '/auth/login';
+        }
         return Promise.reject(error);
     }
 );
@@ -45,11 +48,6 @@ axiosInstance.interceptors.response.use(
 export const handleApiError = (error: unknown): void => {
     if (axios.isAxiosError(error)) {
         console.error('API Error:', error.response?.data);
-        // 여기서 토큰 관련 오류를 확인하고 필요한 경우 로그아웃 처리를 할 수 있습니다.
-        if (error.response?.status === 401) {
-            localStorage.removeItem('Authorization');
-            window.location.href = '/auth/login';
-        }
     } else {
         console.error('Unknown Error:', error);
     }
