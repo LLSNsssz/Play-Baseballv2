f92fb207bb02295487a9d122963f9f78b852510f
LLSNsssz <seungjang93@gmail.com>
2024-10-01 21:28:59 +0900

    Refactor: 토큰관련 전역설정을 위한 코드 추가

--- fe/play-baseball-fe/pages/_app.tsx
+++ fe/play-baseball-fe/pages/_app.tsx
@@ -3,36 +3,39 @@ import { AppProps } from "next/app";
 import Header from "../components/header";
 import { CssBaseline, Container, GlobalStyles } from "@mui/material";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { AuthProvider } from '@/components/AuthContext';
 
 const theme = createTheme();
 
 const globalStyles = (
-  <GlobalStyles
-    styles={{
-      body: {
-        backgroundColor: "#F0F0F0",
-        margin: 0,
-        padding: 0,
-        boxSizing: "border-box",
-      },
-      a: {
-        textDecoration: "none",
-      },
-    }}
-  />
+    <GlobalStyles
+        styles={{
+            body: {
+                backgroundColor: "#F0F0F0",
+                margin: 0,
+                padding: 0,
+                boxSizing: "border-box",
+            },
+            a: {
+                textDecoration: "none",
+            },
+        }}
+    />
 );
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Header />
-      {globalStyles}
-      <Container>
-        <Component {...pageProps} />
-      </Container>
-    </ThemeProvider>
-  );
+    return (
+        <AuthProvider>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <Header />
+                {globalStyles}
+                <Container>
+                    <Component {...pageProps} />
+                </Container>
+            </ThemeProvider>
+        </AuthProvider>
+    );
 };
 
 export default MyApp;
