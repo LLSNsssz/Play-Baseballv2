731b97c571f66ad9c670592b4cace4db2e5af025
LLSNsssz <seungjang93@gmail.com>
2024-10-05 12:00:00 +0900

    Refactor: axios.ts 리프레시토큰 관련 코드 삭제

--- fe/play-baseball-fe/constants/axios.ts
+++ fe/play-baseball-fe/constants/axios.ts
@@ -1,88 +1,41 @@
-import axios, { AxiosError, AxiosInstance, AxiosRequestConfig, InternalAxiosRequestConfig } from 'axios';
+import axios, { AxiosError, AxiosInstance, InternalAxiosRequestConfig } from 'axios';
 import { SERVER_URL, MEMBER_LOGIN } from './endpoints';
-
-interface RetryableAxiosRequestConfig extends AxiosRequestConfig {
-    _retry?: boolean;
-}
 
 const api: AxiosInstance = axios.create({
     baseURL: SERVER_URL,
     withCredentials: true,
 });
 
-let isRefreshing = false;
-let failedQueue: Array<{
-    resolve: (value?: unknown) => void;
-    reject: (reason?: any) => void;
-}> = [];
-
-const processQueue = (error: AxiosError | null, token: string | null = null) => {
-    failedQueue.forEach(prom => {
-        if (error) {
-            prom.reject(error);
-        } else {
-            prom.resolve(token);
-        }
-    });
-    failedQueue = [];
-};
-
-api.interceptors.request.use(
-    (config: InternalAxiosRequestConfig) => {
+axios.interceptors.request.use(
+    (config) => {
         const token = localStorage.getItem('Authorization');
         if (token) {
-            config.headers['Authorization'] = token;
+            config.headers['Authorization'] = token.startsWith('Bearer ') ? token : `Bearer ${token}`;
         }
         return config;
     },
-    (error: AxiosError) => Promise.reject(error)
+    (error) => {
+        console.error("Axios request error:", error);
+        return Promise.reject(error);
+    }
 );
 
 api.interceptors.response.use(
-    (response) => response,
-    async (error: AxiosError) => {
-        const originalRequest = error.config as RetryableAxiosRequestConfig;
-        if (error.response?.status === 401 && originalRequest && !originalRequest._retry) {
-            if (isRefreshing) {
-                return new Promise((resolve, reject) => {
-                    failedQueue.push({resolve, reject});
-                }).then(token => {
-                    if (typeof token === 'string' && originalRequest.headers) {
-                        originalRequest.headers['Authorization'] = 'Bearer ' + token;
-                    }
-                    return api(originalRequest);
-                }).catch(err => {
-                    return Promise.reject(err);
-                });
-            }
-
-            originalRequest._retry = true;
-            isRefreshing = true;
-
-            return new Promise((resolve, reject) => {
-                api.post('/auth/refresh', {}, { withCredentials: true })
-                    .then(({data}) => {
-                        const newToken = data.accessToken;
-                        localStorage.setItem('Authorization', 'Bearer ' + newToken);
-                        api.defaults.headers.common['Authorization'] = 'Bearer ' + newToken;
-                        if (originalRequest.headers) {
-                            originalRequest.headers['Authorization'] = 'Bearer ' + newToken;
-                        }
-                        processQueue(null, newToken);
-                        resolve(api(originalRequest));
-                    })
-                    .catch((err) => {
-                        processQueue(err, null);
-                        localStorage.removeItem('Authorization');
-                        window.location.href = MEMBER_LOGIN;
-                        reject(err);
-                    })
-                    .finally(() => {
-                        isRefreshing = false;
-                    });
-            });
+    (response) => {
+        const newToken = response.headers['authorization'];
+        if (newToken) {
+            // Remove any existing 'Bearer ' prefix before storing
+            const tokenWithoutBearer = newToken.replace(/^Bearer\s+/, '');
+            localStorage.setItem('Authorization', tokenWithoutBearer);
+            api.defaults.headers.common['Authorization'] = `Bearer ${tokenWithoutBearer}`;
         }
-
+        return response;
+    },
+    (error: AxiosError) => {
+        if (error.response?.status === 401) {
+            localStorage.removeItem('Authorization');
+            window.location.href = MEMBER_LOGIN;
+        }
         return Promise.reject(error);
     }
 );
