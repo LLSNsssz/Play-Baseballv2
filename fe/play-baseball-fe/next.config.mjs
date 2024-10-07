3051103f3a9bb287e41d2dd9aab613f2c9931452
LLSNsssz <seungjang93@gmail.com>
2024-10-07 19:00:16 +0900

    Feat: NextJs Config rewrite 룰 추가

--- fe/play-baseball-fe/next.config.mjs
+++ fe/play-baseball-fe/next.config.mjs
@@ -1,8 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
-  images:{
-    domains:['second-inning-bucket-1.s3.ap-northeast-2.amazonaws.com'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'second-inning-bucket-1.s3.ap-northeast-2.amazonaws.com',
+      },
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: process.env.NEXT_PUBLIC_API_URL + '/api/:path*', // 백엔드 서버 URL
+      },
+    ]
   },
 };
 
